404fefbcbbe881c33aa5858578dfd43223a5dfd7
voden6 <voden6@yahoo.com>
2018-09-15 14:17:31 -0400

    first commit

---
+++ create-directories.ps1
@@ -0,0 +1,9 @@
+﻿function Create-Directories{
+    param(
+         [string] $itemPath = '.',
+         [string] $itemName = 'Item1',
+         [int] $i = 1
+         )
+
+    for($i; $i -le 2; $i++) {New-Item -Path $itemPath -ItemType Directory -name $itemName -WhatIf}
+}
